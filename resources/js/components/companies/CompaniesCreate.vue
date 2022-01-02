d7d4104e72632b709b571bd015e6039d2731fdcc
Edo Khudeeda <eido.khudyda@gmail.com>
2022-01-02 15:57:56 +0300

    this is for compleeting

--- resources/js/components/companies/CompaniesCreate.vue
+++ resources/js/components/companies/CompaniesCreate.vue
@@ -1,56 +1,216 @@
 <template>
-   <div v-if="errors">
-  <div v-for="(v, k) in errors" :key="k" class="bg-red-500 text-white rounded font-bold mb-4 shadow-lg py-2 px-4 pr-0">
-    <p v-for="error in v" :key="error" class="text-sm">
-      {{ error }}
-    </p>
+  <div v-if="errors">
+    <div
+      v-for="(v, k) in errors"
+      :key="k"
+      class="
+        bg-red-500
+        text-white
+        rounded
+        font-bold
+        mb-4
+        shadow-lg
+        py-2
+        px-4
+        pr-0
+      "
+    >
+      <p v-for="error in v" :key="error" class="text-sm">
+        {{ error }}
+      </p>
+    </div>
   </div>
-</div>
 
-    <form class="space-y-6" @submit.prevent="saveCompany">
-        <div class="space-y-4 rounded-md shadow-sm">
-            <div>
-                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
-                <div class="mt-1">
-                    <input type="text" name="name" id="name"
-                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                           v-model="form.name">
-                </div>
-            </div>
-
-            <div>
-                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-                <div class="mt-1">
-                    <input type="text" name="email" id="email"
-                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                           v-model="form.email">
-                </div>
-            </div>
-
-            <div>
-                <label for="address" class="block text-sm font-medium text-gray-700">Address</label>
-                <div class="mt-1">
-                    <input type="text" name="address" id="address"
-                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                           v-model="form.address">
-                </div>
-            </div>
-
-            <div>
-                <label for="website" class="block text-sm font-medium text-gray-700">Website</label>
-                <div class="mt-1">
-                    <input type="text" name="website" id="website"
-                           class="block mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                           v-model="form.website">
-                </div>
-            </div>
+  <form class="space-y-6" @submit.prevent="saveCompany">
+    <div class="space-y-4 rounded-md shadow-sm">
+      <div>
+        <label for="title" class="block text-sm font-medium text-gray-700"
+          >Title</label
+        >
+        <div class="mt-1">
+          <input
+            type="text"
+            name="title"
+            id="title"
+            class="
+              block
+              mt-1
+              w-full
+              rounded-md
+              border-gray-300
+              shadow-sm
+              focus:border-indigo-300
+              focus:ring
+              focus:ring-indigo-200
+              focus:ring-opacity-50
+            "
+            v-model="form.title"
+          />
         </div>
+      </div>
 
-        <button type="submit"
-                class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase bg-gray-800 rounded-md border border-transparent ring-gray-300 transition duration-150 ease-in-out hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring disabled:opacity-25">
-            Create
-        </button>
-    </form>
+      <div>
+        <label for="description" class="block text-sm font-medium text-gray-700"
+          >Description</label
+        >
+        <div class="mt-1">
+          <input
+            type="text"
+            name="description"
+            id="description"
+            class="
+              block
+              mt-1
+              w-full
+              rounded-md
+              border-gray-300
+              shadow-sm
+              focus:border-indigo-300
+              focus:ring
+              focus:ring-indigo-200
+              focus:ring-opacity-50
+            "
+            v-model="form.description"
+          />
+        </div>
+      </div>
+      <div>
+        <label for="client" class="block text-sm font-medium text-gray-700"
+          >Client</label
+        >
+        <div class="mt-1">
+          <input
+            type="text"
+            name="client"
+            id="client"
+            class="
+              block
+              mt-1
+              w-full
+              rounded-md
+              border-gray-300
+              shadow-sm
+              focus:border-indigo-300
+              focus:ring
+              focus:ring-indigo-200
+              focus:ring-opacity-50
+            "
+            v-model="form.client"
+          />
+        </div>
+      </div>
+
+      <div>
+        <label for="project_url" class="block text-sm font-medium text-gray-700"
+          >Project URL</label
+        >
+        <div class="mt-1">
+          <input
+            type="url"
+            name="project_url"
+            id="project_url"
+            class="
+              block
+              mt-1
+              w-full
+              rounded-md
+              border-gray-300
+              shadow-sm
+              focus:border-indigo-300
+              focus:ring
+              focus:ring-indigo-200
+              focus:ring-opacity-50
+            "
+            v-model="form.project_url"
+          />
+        </div>
+      </div>
+
+      <div>
+        <label for="website" class="block text-sm font-medium text-gray-700"
+          >Category</label
+        >
+        <div class="mt-1">
+          <!-- <input
+            type="text"
+            name="website"
+            id="website"
+            class="
+              block
+              mt-1
+              w-full
+              rounded-md
+              border-gray-300
+              shadow-sm
+              focus:border-indigo-300
+              focus:ring
+              focus:ring-indigo-200
+              focus:ring-opacity-50
+            "
+            v-model="form.website"
+          /> -->
+          <select
+            name="category"
+            id="category"
+            class="
+              block
+              mt-1
+              w-full
+              rounded-md
+              border-gray-300
+              shadow-sm
+              focus:border-indigo-300
+              focus:ring
+              focus:ring-indigo-200
+              focus:ring-opacity-50
+            "
+            v-model="form.category"
+          >
+            <option value="mobile">Mobile</option>
+            <option value="web">Web</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <label for="website" class="block text-sm font-medium text-gray-700"
+          >Image</label
+        >
+        <div class="mt-1">
+          <input type="file" @change="onFileSelected" />
+        </div>
+        <!-- <button>Upload</button> -->
+      </div>
+    </div>
+
+    <button
+      type="submit"
+      class="
+        inline-flex
+        items-center
+        px-4
+        py-2
+        text-xs
+        font-semibold
+        tracking-widest
+        text-white
+        uppercase
+        bg-gray-800
+        rounded-md
+        border border-transparent
+        ring-gray-300
+        transition
+        duration-150
+        ease-in-out
+        hover:bg-gray-700
+        active:bg-gray-900
+        focus:outline-none focus:border-gray-900 focus:ring
+        disabled:opacity-25
+      "
+    >
+      Create
+    </button>
+  </form>
+  {{ form }}
 </template>
 
 <script>
@@ -58,25 +218,36 @@ import { reactive } from "vue";
 import useCompanies from "../../composables/companies";
 
 export default {
-    setup() {
-        const form = reactive({
-            'name': '',
-            'email': '',
-            'address': '',
-            'website': '',
-        })
+  setup() {
+    const form = reactive({
+      title: "",
+      description: "",
+      image: "",
+      category: "",
+      client: "",
+      project_url: "",
+    });
+    let selectedFile = reactive(null);
 
-        const { errors, storeCompany } = useCompanies()
+    const { errors, storeCompany } = useCompanies();
 
-        const saveCompany = async () => {
-            await storeCompany({...form});
-        }
-
-        return {
-            form,
-            errors,
-            saveCompany
-        }
+    const saveCompany = async () => {
+      onUpload();
+      await storeCompany({ ...form });
+    };
+    function onFileSelected(event) {
+      // selectedFile=event.target.files[0]
+      form.image = event.target.files[0].name;
+      console.log(form.image);
     }
-}
+    function onUpload() {}
+
+    return {
+      form,
+      errors,
+      saveCompany,
+      onFileSelected,
+    };
+  },
+};
 </script>
