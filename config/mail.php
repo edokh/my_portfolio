4b39b082b51af7f4593bd7730f83783c86560928
Edo Khudeeda <eido.khudyda@gmail.com>
2022-02-05 10:45:01 +0300

    jj

--- config/mail.php
+++ config/mail.php
@@ -34,27 +34,16 @@ return [
     */
 
     'mailers' => [
-        // 'smtp' => [
-        //     'transport' => 'smtp',
-        //     'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-        //     'port' => env('MAIL_PORT', 587),
-        //     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-        //     'username' => env('MAIL_USERNAME'),
-        //     'password' => env('MAIL_PASSWORD'),
-        //     'timeout' => null,
-        //     'auth_mode' => null,
-        // ],
         'smtp' => [
             'transport' => 'smtp',
-            'host' => 'smtp.googlemail.com',
-            'port' => 465,
-            'encryption' => 'ssl',
-            'username' => 'eido.khudyda@gmail.com',
-            'password' => '8169157841Edo',
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
         ],
-
         'ses' => [
             'transport' => 'ses',
         ],
